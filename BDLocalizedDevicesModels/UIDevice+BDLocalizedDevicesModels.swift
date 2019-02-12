9e60e145dd067ed56020768f57cbd40ee0dd135a
Benoit Deldicque <bixcorp@gmail.com>
2019-02-12 20:56:43 +0100

    Cleaned code

--- BDLocalizedDevicesModels/UIDevice+BDLocalizedDevicesModels.swift
+++ BDLocalizedDevicesModels/UIDevice+BDLocalizedDevicesModels.swift
@@ -13,47 +13,41 @@ public typealias BDDevice = UIDevice
 public extension BDDevice {
     /// The product name of the device.
     @objc public var productName: String {
-        get {
-            // Retrieve english bundle.
-            guard let englishPath = self.frameworkBundle.path(forResource: "en", ofType: "lproj") else {
-                return self.localizedProductName // Fallback on localized model
-            }
-
-            guard let englishBundle = Bundle(path: englishPath) else {
-                return self.localizedProductName // Fallback on localized model
-            }
-
-            return NSLocalizedString(self.deviceTypeIdentifier, tableName: "DeviceModel", bundle: englishBundle, value: self.deviceTypeIdentifier, comment: "")
+        // Retrieve english bundle.
+        guard let englishPath = self.frameworkBundle.path(forResource: "en", ofType: "lproj") else {
+            return self.localizedProductName // Fallback on localized model
         }
+
+        guard let englishBundle = Bundle(path: englishPath) else {
+            return self.localizedProductName // Fallback on localized model
+        }
+
+        return NSLocalizedString(self.deviceTypeIdentifier, tableName: "DeviceModel",
+                                 bundle: englishBundle, value: self.deviceTypeIdentifier, comment: "")
     }
 
     /// The product name of the device as a localized string.
     @objc public var localizedProductName: String {
-        get {
-            return NSLocalizedString(self.deviceTypeIdentifier, tableName: "DeviceModel", bundle: self.frameworkBundle, value: self.deviceTypeIdentifier, comment: "")
-        }
+        return NSLocalizedString(self.deviceTypeIdentifier, tableName: "DeviceModel",
+                                 bundle: self.frameworkBundle, value: self.deviceTypeIdentifier, comment: "")
     }
 
     // MARK: -
     private var deviceTypeIdentifier: String {
-        get {
-            // Check if device is a simulator to get the right machine identifier.
-            if let machine = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] {
-                return machine
-            } else {
-                var size: Int = 0
-                sysctlbyname("hw.machine", nil, &size, nil, 0)
-                var machine = [CChar](repeating: 0, count: size)
-                sysctlbyname("hw.machine", &machine, &size, nil, 0)
+        // Check if device is a simulator to get the right machine identifier.
+        if let machine = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] {
+            return machine
+        } else {
+            var size: Int = 0
+            sysctlbyname("hw.machine", nil, &size, nil, 0)
+            var machine = [CChar](repeating: 0, count: size)
+            sysctlbyname("hw.machine", &machine, &size, nil, 0)
 
-                return String(cString: machine)
-            }
+            return String(cString: machine)
         }
     }
 
     private var frameworkBundle: Bundle {
-        get {
-            return Bundle(identifier: "com.bddq.BDLocalizedDevicesModels")!
-        }
+        return Bundle(identifier: "com.bddq.BDLocalizedDevicesModels")!
     }
 }
--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -23,7 +23,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
